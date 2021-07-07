b6132c08ac25b5a3c7734427e5eddcc7884b0fcd
dzevakov <dmitryzev@gmail.com>
2021-07-07 19:43:27 +0300

    feat: add some styles for app block wrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './App.css';
+import './App.css'
+import TodoItem from './todo-item/todo-item';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <header className="App-header">
        <h1>ToDo App</h1>
       </header>
+      <section className={'wrapper-app'}>
+        <TodoItem />
+      </section>
     </div>
   );
 }
