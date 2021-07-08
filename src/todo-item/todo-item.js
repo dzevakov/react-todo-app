a888e6ad1c100442082de40cc71e9b302023156c
dzevakov <dmityrzev@gmail.com>
2021-07-08 22:43:51 +0300

    feat: add FontAwesome icons for UI buttons

--- src/todo-item/todo-item.js
+++ src/todo-item/todo-item.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './todo-item.scss'
 import PropTypes from 'prop-types'
 import DeleteContext from '../context/context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faEdit, faSave } from '@fortawesome/free-solid-svg-icons'
 
 function TodoItem(props) {
   const [taskIsEdit, setTaskIsEdit] = React.useState(false)
@@ -48,13 +50,17 @@ function TodoItem(props) {
         className={'task-block__button task-block__button_edit'}
         onClick={taskEditHandler}
       >
-        {
-          taskIsEdit ? 'save' : 'edit'
-        }
+        {taskIsEdit ? (
+          <FontAwesomeIcon icon={faSave} style={{color: '#2897ed'}}/>
+        ) : (
+          <FontAwesomeIcon icon={faEdit} />
+        )}
       </button>
-      <button className={'task-block__button task-block__button_delete'}
-        onClick={() => deleteTodo(props.id)}>
-        delete
+      <button
+        className={'task-block__button task-block__button_delete'}
+        onClick={() => deleteTodo(props.id)}
+      >
+        <FontAwesomeIcon icon={faTrashAlt} style={{ color: '#f63a0f' }} />
       </button>
     </div>
   )
@@ -65,7 +71,7 @@ TodoItem.propTypes = {
   taskName: PropTypes.string,
   isDone: PropTypes.bool,
   onChangeName: PropTypes.func,
-  onToggle: PropTypes.func
+  onToggle: PropTypes.func,
 }
 
 export default TodoItem
